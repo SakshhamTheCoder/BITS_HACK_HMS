3d55e88a26c1c4b2fdce1b139236dfc8c4fcae38
ShreeMishraa <smishra_be23@thapar.edu>
2025-01-09 19:25:07 +0530

    frontend changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-teal-500 text-white py-2">
+    <footer className="bg-teal-600 text-white py-2">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 Hospital Management</p>
       </div>
